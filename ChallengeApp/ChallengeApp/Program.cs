3d61e857f47c898e84c31a9760b96e0bc354232f
Karol Radzikowski <karol.radzikowski12@gmail.com>
2023-04-15 13:34:32 +0200

    zadanie domowe dzień 4

--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿var name = "Ewa";
+var Age = 33;
+bool kobieta = true;
+var resualt1 = "Kobirta poniżej 30 lat";
+var resualt2 = "Ewa lat 33";
+var resualt3 = "Mężczyzna nie pełnoletni";
+
+if (kobieta == false)
+{
+    Console.WriteLine(resualt3);
+}
+if (kobieta == true && Age < 30)
+{
+    Console.WriteLine(resualt1);
+}
+else if (name == "Ewa" && Age == 33)
+{
+    Console.WriteLine(resualt2);
+}
