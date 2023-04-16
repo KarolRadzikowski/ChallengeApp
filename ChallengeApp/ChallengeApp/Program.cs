12be3c1764d2639221cceff4fb904278355ff8f9
Karol Radzikowski <karol.radzikowski12@gmail.com>
2023-04-16 12:48:18 +0200

    Zadanie domowe dzień 5

--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -1,32 +1,27 @@
-﻿var Name = "Ewa";
-var Age = 33;
-bool female = true;
-var resault1 = "Kobieta poniżej 30lat";
-var resault2 = "Ewa 33 lat";
-var resault3 = "Niepełnoletni Mężczyzna";
-var resault4 = "Mężczyzna powyżej 50 lat";
+﻿int number = 7665284;
 
-if (female == true)
+string numberInString = number.ToString();
+char[] letters = numberInString.ToCharArray();
+List<char> numbers = new List<char>();
+numbers.Add('0');
+numbers.Add('1');
+numbers.Add('2');
+numbers.Add('3');
+numbers.Add('4');
+numbers.Add('5');
+numbers.Add('6');
+numbers.Add('7');
+numbers.Add('8');
+numbers.Add('9');
+
+foreach (var digiti in numbers)
 {
-    if (Name == "Ewa" && Age == 33)
+    int count = 0;
+    foreach (var sign in letters)
     {
-        Console.WriteLine(resault2);
+        if (digiti == sign)
+        { count++;
+        }
     }
-    else if (!(Name == "Ewa") && Age < 30)
-    {
-        Console.WriteLine(resault1);
-    }
+    Console.WriteLine(digiti + "-lość poszczególnego znaku w zadanej liczbie:" + count);
 }
-
-else
-{
-    if (female == false && Age < 18)
-    {
-        Console.WriteLine(resault3);
-    }
-    else if (female == false && Age < 50)
-    {
-        Console.WriteLine(resault4);
-    }
-}
-
