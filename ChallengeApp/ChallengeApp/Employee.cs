57608d99b03eca9b502fca8945f11a084ec32586
Karol Radzikowski <karol.radzikowski12@gmail.com>
2023-04-18 20:18:28 +0200

    Zadanie dzień 6

---
+++ ChallengeApp/ChallengeApp/Employee.cs
@@ -0,0 +1,43 @@
+﻿
+namespace ChallengeApp
+{
+    public class Employee
+    {
+        public List<int> score = new List<int>();
+
+        
+        
+        
+        
+
+    public Employee(string Name, string Surename, int Age)
+        {
+            this.Name = Name;
+            this.Surname = Surename;
+            this.Age = Age;
+            this.Score = 0;
+            
+        }
+
+
+        public string Name{ get; private set; }
+        public string Surname { get; private set; }
+        public int Age { get; private set; }
+        public int Score { get; private set; }
+        public int Resault
+        {
+            get
+            {
+                return score.Sum();
+            }
+        }
+
+        private List<int> scoreList = new List<int>();
+
+        public void AddScore(int rating)
+        {
+            this.Score += rating;
+        }
+
+    }
+}
--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -1,27 +1,53 @@
-﻿int number = 7665284;
+﻿using ChallengeApp;
 
-string numberInString = number.ToString();
-char[] letters = numberInString.ToCharArray();
-List<char> numbers = new List<char>();
-numbers.Add('0');
-numbers.Add('1');
-numbers.Add('2');
-numbers.Add('3');
-numbers.Add('4');
-numbers.Add('5');
-numbers.Add('6');
-numbers.Add('7');
-numbers.Add('8');
-numbers.Add('9');
+Employee employee1 = new Employee("Karol", "Radzikowski", 30);
+Employee employee2 = new Employee("Alicja", "Radzikowska", 26);
+Employee employee3 = new Employee("Klara", "Radzikowska", 3);
 
-foreach (var digiti in numbers)
+employee1.AddScore(5);
+employee1.AddScore(2);
+employee1.AddScore(9);
+employee1.AddScore(7);
+employee1.AddScore(8);
+
+
+employee2.AddScore(9);
+employee2.AddScore(3);
+employee2.AddScore(1);
+employee2.AddScore(2);
+employee2.AddScore(10);
+
+
+
+employee3.AddScore(4);
+employee3.AddScore(5);
+employee3.AddScore(1);
+employee3.AddScore(2);
+employee3.AddScore(6);
+
+List<Employee> employees = new List<Employee>()
 {
-    int count = 0;
-    foreach (var sign in letters)
+    employee1, employee2, employee3
+    };
+int maxScore = -1;
+Employee employeeTopScore = null;
+
+foreach (var employee in employees)
+{
+    if (employee.Score > maxScore)
     {
-        if (digiti == sign)
-        { count++;
-        }
+        maxScore = employee.Score; 
+        employeeTopScore = employee;
     }
-    Console.WriteLine(digiti + "-lość poszczególnego znaku w zadanej liczbie:" + count);
 }
+
+
+
+Console.WriteLine("Imię:" + employeeTopScore.Name);
+Console.WriteLine("Nazwisko:" + employeeTopScore.Surname);
+Console.WriteLine("Wiek:" + employeeTopScore.Age + "Lat");
+Console.WriteLine("Ilość punktów:" + employeeTopScore.Score + "Pkt");
+
+
+
+
